c1b0891b78d643c048db67102115d7e71a21aed4
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-08-01 11:08:15 -0500

    Add project files.

---
+++ Mask-Sensitive-Info/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Mask_Sensitive_Info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tell me a secret and I will hide it for you.");
+
+            var userInput = Console.ReadLine();
+
+            var maskedSecret = new String('X', userInput.Length - 4) + userInput.Substring(userInput.Length - 4);
+
+            Console.WriteLine(maskedSecret);
+
+            Console.ReadKey();
+        }
+    }
+}
